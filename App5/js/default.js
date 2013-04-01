5414b655b17d8dc1ff055d2037d9115cd3900171
Ming-der Wang <mingder78@yahoo.com>
2013-04-01 17:42:12 +0800

    2 stages switching.

--- App5/js/default.js
+++ App5/js/default.js
@@ -23,79 +23,64 @@
                 nav.history = app.sessionState.history;
             }
 
-            args.setPromise(WinJS.UI.processAll().then(function () {
-                if (nav.location) {
-                    nav.history.current.initialPlaceholder = true;
-                    return nav.navigate(nav.location, nav.state);
-                } else {
-                    return nav.navigate(Application.navigator.home);
-                }
-            }).then(init));
+            args.setPromise(WinJS.UI.processAll().then(init));
         }
     });
 
-    var stage;
     function init() {
+        stage1();
+    }
+
+    function stage1() {
+        var mc1 = document.getElementById("video1");
+        mc1.src = "movies/1080_1920_20s.mp4";
+        mc1.width = 1080;
+        mc1.height = 1920;
+        mc1.addEventListener("MSPointerUp", stage2, false);
+        var myTimeline = new TimelineLite();
+
+        var mc2 = document.getElementById("video2");
+
+
+        //this will have 3 tween starting one after the one before is done
+        myTimeline.append(new TweenMax(mc1, 0, { y: 0, x: -1080 }));
+        myTimeline.append(new TweenMax(mc1, 2, { y: 0, x: 0 }));
+        myTimeline.play();
+        
+    }
+    function stage2() {
         var mc1 = document.getElementById("video1");
         mc1.src = "movies/1080_640_20s.mp4";
+        mc1.width = 1080;
+        mc1.height = 640;
+        mc1.removeEventListener("MSPointerUp", stage2, false);
+        mc1.addEventListener("MSPointerUp", stage1, false);
+
         var mc2 = document.getElementById("video2");
         mc2.src = "http://www.gopicfreak.com/wp-content/uploads/2013/03/beautiful-girl-wallpaper-115.jpg";
         var mc3 = document.getElementById("contenthost");
         var myTimeline = new TimelineLite();
- 
+
         //this will have 3 tween starting one after the one before is done
-        myTimeline.append(new TweenMax(mc1, 0, { y: 0, x: -1080 }));     
+        myTimeline.append(new TweenMax(mc1, 0, { y: 0, x: -1080 }));
         myTimeline.append(new TweenMax(mc2, 0, { y: 0, x: -1080 }));
         myTimeline.append(new TweenMax(mc3, 0, { y: 0, x: -1080 }));
         myTimeline.append(new TweenMax(mc1, 2, { y: 0, x: 0 }));
-        myTimeline.append(new TweenMax(mc2, 1, { y: 0, x: 0, repeat: 2 }));
-        myTimeline.append(new TweenMax(mc3, 2, { bezier: [{ x: 0, y:640 }, { x: 0, y: 0 }], orientToBezier: true, ease: Bounce.easeOut }));
- 
+        myTimeline.append(new TweenMax(mc2, 1, { y: 0, x: 0 }));
+        myTimeline.append(new TweenMax(mc3, 2, { y: 0, x: 0 , ease: Bounce.easeOut }));
+//        myTimeline.append(new TweenMax(mc3, 2, { bezier: [{ x: 0, y: 640 }, { x: 0, y: 0 }], orientToBezier: true, ease: Bounce.easeOut }));
+
         myTimeline.play();
         // for (var i:int = 0; i < gridBoxes.length; i++) { }
 
-        stage = new createjs.Stage("gameCanvas");
-        createjs.Ticker.addEventListener("tick", stage);
-
-        // Create the MovieClip
-        var mc = new createjs.MovieClip(null, 0, true, {
-            start: 0,
-            middle: 40
-        });
-        stage.addChild(mc);
-
-        // Create the States. Each state is just a circle shape.
-        var state1 = new createjs.Shape(
-        new createjs.Graphics().beginFill("#999999")
-            .drawCircle(100, 100, 100));
-        var state2 = new createjs.Shape(
-        new createjs.Graphics().beginFill("#5a9cfb")
-            .drawCircle(100, 100, 100));
-
-        // Create a tween for each shape, animating from one side to the other.
-        mc.timeline.addTween(
-        createjs.Tween.get(state1)
-            .to({
-                x: 0
-            }).to({
-                x: 880
-            }, 40).to({
-                x: 0
-            }, 40));
-        mc.timeline.addTween(
-        createjs.Tween.get(state2)
-            .to({
-                x: 880
-            }).to({
-                x: 0
-            }, 40).to({
-                x: 880
-            }, 40));
-
-        // Play the animation starting from the middle. See the MovieClip constructor above where the labels are specified.
-        mc.gotoAndPlay("middle");
-    }
-
+        // for WinJS navigator
+        if (nav.location) {
+            nav.history.current.initialPlaceholder = true;
+            return nav.navigate(nav.location, nav.state);
+        } else {
+            return nav.navigate(Application.navigator.home);
+        }
+    }
     app.oncheckpoint = function (args) {
         // TODO: This application is about to be suspended. Save any state
         // that needs to persist across suspensions here. If you need to 
